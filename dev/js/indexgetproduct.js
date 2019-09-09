d2e36d414356c5d81151b60402fd370d31455742
Frenkieli <sd789852t@gmail.com>
2019-09-09 18:06:08 +0800

    首頁商店完成

--- dev/js/indexgetproduct.js
+++ dev/js/indexgetproduct.js
@@ -16,7 +16,7 @@
             }
         }
         var url = "php/getProduct_JSON.php";
-        xhr.open('get', url, true);
+        xhr.open('get', url, false);
         xhr.send(null);
     }
     function showproduct(jsonStr) {
@@ -40,12 +40,23 @@
     function pushProduct() {
         let productIn;
         for (let i = 0; i < 12; i++) {
-            productIn = arr.splice(`${Math.floor(Math.random() * arr.length)}`, 1);
-            console.log(productIn , '印商品');
+            productIn = arr.splice(`${Math.floor(Math.random() * arr.length)}`, 1).join(',').split(',');
+            // console.log(arr,'陣列');
+            // console.log(productIn , '印商品');
             document.querySelectorAll('.sort_same_item_img img')[i].src = productIn[2];
             document.querySelectorAll('.sort_same_item_title')[i].innerHTML = productIn[1];
             document.querySelectorAll('.sort_same_item_price')[i].innerHTML = 'NT.'+productIn[4];
-            // document.querySelectorAll('.swiper-slide.gray_linear_gradient.center')[i].backgroundImage = productIn[7];
+            console.log(productIn[7]);
+            if(productIn[7] == '仙人掌'){
+                document.querySelectorAll('.swiper-slide.gray_linear_gradient.center')[i].classList.add('card_bg1');
+                console.log('有聘來');
+            }else if(productIn[7] == '老鷹'){
+                document.querySelectorAll('.swiper-slide.gray_linear_gradient.center')[i].classList.add('card_bg3');
+                console.log('有聘來');
+            }else if(productIn[7] == '恐龍'){
+                document.querySelectorAll('.swiper-slide.gray_linear_gradient.center')[i].classList.add('card_bg2');
+                console.log('有聘來');
+            }
         }
     }
 })();
