fd945ff08fad6fffd72ae04163d7e83a0c07cf41
Rex Lin <gerd3317@gmail.com>
2019-09-16 02:04:25 +0800

    新增星星評比(不含php)

---
+++ dev/js/starBtnLevel.js
@@ -0,0 +1,20 @@
+$(document).on('click', '.1star', function(){
+    let starToChange =document.getElementById("starLevel");
+    starToChange.src="../dest/images/information/other/1star.svg";
+})
+$(document).on('click', '.2star', function(){
+    let starToChange =document.getElementById("starLevel");
+    starToChange.src="../dest/images/information/other/2star.svg";
+})
+$(document).on('click', '.3star', function(){
+    let starToChange =document.getElementById("starLevel");
+    starToChange.src="../dest/images/information/other/3star.svg";
+})
+$(document).on('click', '.4star', function(){
+    let starToChange =document.getElementById("starLevel");
+    starToChange.src="../dest/images/information/other/4star.svg";
+})
+$(document).on('click', '.5star', function(){
+    let starToChange =document.getElementById("starLevel");
+    starToChange.src="../dest/images/information/other/5star.svg";
+})
