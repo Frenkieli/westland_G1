b1c422cce134e44614d0e86ed9548d613ff134c0
libby0510 <libby750510@gmail.com>
2019-09-16 13:45:19 +0800

    會員 登入 活動PHP 會員登入PHP 首頁登入JS

---
+++ dev/php/getactivity_JSON.php
@@ -0,0 +1,22 @@
+
+<?php
+// <!-- ====活動資訊==== -->
+
+try{
+    require_once("connectWestland.php"); //引用連線
+    $sql = "select * from activity" ;
+    $activitys =$pdo->prepare($sql);
+    $activitys->execute();
+    if($activitys->rowCount()==0){
+        echo "{}";
+    }else{
+        //=====撈資料=====
+        $activityRow=$activitys->fetchAll(PDO::FETCH_ASSOC);
+        //=====轉成json格式回傳=====
+        echo json_encode($activityRow);
+    }
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
+
+?>
