d2ccc237c6ef0eb7bec7b68b763dc568021c1bad
Frenkieli <sd789852t@gmail.com>
2019-09-17 17:36:54 +0800

    遊玩頁面基本功能完成(含手機加分)

--- dev/js/gameplayVue.js
+++ dev/js/gameplayVue.js
@@ -1,5 +1,6 @@
 localStorage['member_useticket'] = null;
 let memTicket = [];
+var ticketScore;
 
 getTicket();
 function getTicket() {
@@ -93,13 +94,18 @@ function showTicket(jsonStr) {
         let index = 0;
         for (let j in ticket[i]) {
             // console.log(ticket[i][j]);
+            if(j =='team_name' & !ticket[i][j]){
+                ticket[i][j] = '無隊伍';
+                // console.log(ticket[i][j],'這是什麼');
+            }
             forticket[index] = (ticket[i][j]);
             index++;
 
         }
         memTicket[i] = forticket;
+        // console.log('確認',memTicket);
     }
-    console.log(memTicket);
+    // console.log(memTicket);
     if (memTicket[0][2]) {
         localStorage['member_useticket'] = memTicket[0][2];
     }
@@ -140,6 +146,7 @@ function setTicket(jsonStr) {
     document.querySelector('.ticket_name p').innerHTML = ticket.member_name;
     document.querySelector('.ticket_img img').src = ticket.image_source;
     document.querySelector('.ticket_team p').innerHTML = ticket.team_name;
+    ticketScore = ticket.bounty;
     document.querySelector('.ticket_reward p').innerHTML = '$' + ticket.bounty;
     document.querySelector('.ticket_mascot').src = ticket.mascot_image;
     if (ticket.mission_no) {
@@ -167,8 +174,8 @@ function setTicketmisiion(missionNum, mission_no) {
                 let missinDiv = document.querySelectorAll('.progress_box');
                 for (i = 0; i < missinName.length; i++) {
                     console.log(section_titleText, '+++++');
-                    for(let j =0 ; j<section_titleText.length;j++){
-                        if(section_titleText[j].innerHTML ==missinName[i].equ_name){
+                    for (let j = 0; j < section_titleText.length; j++) {
+                        if (section_titleText[j].innerHTML == missinName[i].equ_name) {
                             section_titleText[j].innerHTML += '(任務)';
                         }
                     }
@@ -199,64 +206,132 @@ let demo_scaninner = document.querySelectorAll('.demo_scaninner .btn');
 for (let i = 0; i < demo_scaninner.length; i++) {
     demo_scaninner[i].addEventListener('click', addPointsCheck, false);
 }
+var facilityPointsCheck = 0;
+function addPointsCheck(value) {
+    // console.log(value.length);
+    if (value.length > 2) {
+        switch (value) {
+            case 'redeem_product_status':
+                console.log('redeem_product_status');
+                facilityPoint('商店', 'redeem_product_status');
+                if (facilityPointsCheck * 1) {
+                    facilityPointsCheck = 0;
+                }else{
+                    alert('要超過６００分才能換商品喔！');
+                }
+                break;
+            case 'waterslide':
+                console.log('waterslide');
+                addPoint('滑水道', 'waterslide');
+                break;
+            case 'swivel_chair':
+                console.log('swivel_chair');
+                addPoint('旋轉椅', 'swivel_chair');
+                break;
+            case 'pirate_ship':
+                console.log('pirate_ship');
+                addPoint('海盜船', 'pirate_ship');
+                break;
+            case 'entrance_status':
+                console.log('entrance_status');
+                facilityPoint('入口', 'entrance_status');
+                break;
+            case 'roller_coaster':
+                console.log('roller_coaster');
+                addPoint('雲霄飛車', 'roller_coaster');
+                break;
+            case 'carousel':
+                console.log('carousel');
+                addPoint('旋轉木馬', 'carousel');
+                break;
+            case 'ferris_wheel':
+                console.log('ferris_wheel');
+                addPoint('摩天輪', 'ferris_wheel');
+                break;
+            case 'exit_status':
+                console.log('exit_status');
+                facilityPoint('出口', 'exit_status');
+                break;
+            default:
+                break;
+        }
+    } else {
 
-function addPointsCheck(e) {
-    console.log($(this).prev().prev().text());
-    switch ($(this).prev().prev().text().replace('(任務)','').replace('(達成)','')) {
-        case '商店':
-            console.log('redeem_product_status');
-            
-            break;
-        case '滑水道':
-            console.log('waterslide');
-            addPoint($(this).prev().prev().text().replace('(任務)','').replace('(達成)',''),'waterslide');
-            break;
-        case '旋轉椅':
-            console.log('swivel_chair');
-            addPoint($(this).prev().prev().text().replace('(任務)','').replace('(達成)',''),'swivel_chair');
-            break;
-        case '海盜船':
-            console.log('pirate_ship');
-            addPoint($(this).prev().prev().text().replace('(任務)','').replace('(達成)',''),'pirate_ship');
-            break;
-        case '入口':
-            console.log('entrance_status');
-
-            break;
-        case '雲霄飛車':
-            console.log('roller_coaster');
-            addPoint($(this).prev().prev().text().replace('(任務)','').replace('(達成)',''),'roller_coaster');
-            break;
-        case '旋轉木馬':
-            console.log('carousel');
-            addPoint($(this).prev().prev().text().replace('(任務)','').replace('(達成)',''),'carousel');
-            break;
-        case '摩天輪':
-            console.log('ferris_wheel');
-            addPoint($(this).prev().prev().text().replace('(任務)','').replace('(達成)',''),'ferris_wheel');
-            break;
-        case '出口':
-            console.log('exit_status');
-
-            break;
-        default:
-            break;
+        console.log($(this).prev().prev().text());
+        switch ($(this).prev().prev().text().replace('(任務)', '').replace('(達成)', '').replace('(玩過囉))', '')) {
+            case '商店':
+                console.log('redeem_product_status');
+                facilityPoint($(this).prev().prev().text(), 'redeem_product_status');
+                if (facilityPointsCheck * 1) {
+                    $(this).prev().prev().text($(this).prev().prev().text() + '(完成)');
+                    facilityPointsCheck = 0;
+                }else{
+                    alert('要超過６００分才能換商品喔！');
+                }
+                break;
+            case '滑水道':
+                console.log('waterslide');
+                addPoint($(this).prev().prev().text().replace('(任務)', '').replace('(達成)', ''), 'waterslide');
+                break;
+            case '旋轉椅':
+                console.log('swivel_chair');
+                addPoint($(this).prev().prev().text().replace('(任務)', '').replace('(達成)', ''), 'swivel_chair');
+                break;
+            case '海盜船':
+                console.log('pirate_ship');
+                addPoint($(this).prev().prev().text().replace('(任務)', '').replace('(達成)', ''), 'pirate_ship');
+                break;
+            case '入口':
+                console.log('entrance_status');
+                facilityPoint($(this).prev().prev().text(), 'entrance_status');
+                $(this).prev().prev().text($(this).prev().prev().text() + '(進場)');
+                break;
+            case '雲霄飛車':
+                console.log('roller_coaster');
+                addPoint($(this).prev().prev().text().replace('(任務)', '').replace('(達成)', ''), 'roller_coaster');
+                break;
+            case '旋轉木馬':
+                console.log('carousel');
+                addPoint($(this).prev().prev().text().replace('(任務)', '').replace('(達成)', ''), 'carousel');
+                break;
+            case '摩天輪':
+                console.log('ferris_wheel');
+                addPoint($(this).prev().prev().text().replace('(任務)', '').replace('(達成)', ''), 'ferris_wheel');
+                break;
+            case '出口':
+                console.log('exit_status');
+                facilityPoint($(this).prev().prev().text(), 'exit_status');
+                outLand();
+                break;
+            default:
+                break;
+        }
+        if($(this).prev().prev().text().indexOf('(任務)') != -1){
+            $(this).prev().prev().text($(this).prev().prev().text().replace('(任務)', '(達成)'));
+        }else if($(this).prev().prev().text().indexOf('(達成)') != -1){
+        }else if($(this).prev().prev().text().indexOf('入口') != -1){
+        }else if($(this).prev().prev().text().indexOf('商店') != -1){
+        }else if($(this).prev().prev().text().indexOf('出口') != -1){
+        }else{
+            $(this).prev().prev().text($(this).prev().prev().text() + '(玩過囉)');
+        }
     }
-    $(this).prev().prev().text($(this).prev().prev().text().replace('(任務)','(達成)'));
 }
 
-function addPoint(str,eStr){
+function addPoint(str, eStr) {
     var xhr = new XMLHttpRequest();
     xhr.onload = function () {
         if (xhr.status == 200) {
             //..................取回server端回傳的使用者資料
             if (xhr.responseText.indexOf("sysError") != -1) {
                 alert("系統異常,請通知系統維護人員");
-            } else if (xhr.responseText.indexOf("loginError") != -1) {
-                alert("帳密錯誤");
+            } else if (xhr.responseText.indexOf("還沒進場喔！") != -1) {
+                alert("還沒進場喔！");
             } else {
                 console.log(xhr.responseText);
-                document.querySelector('.ticket_reward p').innerHTML = '$' + document.querySelector('.ticket_reward p').innerHTML.replace('$' ,'') * 1 + xhr.responseText.split('|')[0];
+                ticketScore = parseInt(ticketScore);
+                ticketScore += parseInt(xhr.responseText);
+                document.querySelector('.ticket_reward p').innerHTML = '$' + ticketScore;
             }
         } else {
             alert(xhr.status);
@@ -265,5 +340,51 @@ function addPoint(str,eStr){
     xhr.open("post", "php/gameplayaddPoints.php", true);
     xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
     var data_info = `str=${str}&eStr=${eStr}&ticket=${localStorage['member_useticket']}`;
+    xhr.send(data_info);
+}
+
+function facilityPoint(str, eStr) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.status == 200) {
+            //..................取回server端回傳的使用者資料
+            if (xhr.responseText.indexOf("sysError") != -1) {
+                alert("系統異常,請通知系統維護人員");
+            } else if (xhr.responseText.indexOf("還沒進場喔！") != -1) {
+                alert("還沒進場喔！");
+            } else {
+                console.log(xhr.responseText);
+                facilityPointsCheck = xhr.responseText * 1;
+            }
+        } else {
+            alert(xhr.status);
+        }
+    }
+    xhr.open("post", "php/getfacilityPoint.php", false);
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    var data_info = `str=${str}&eStr=${eStr}&ticket=${localStorage['member_useticket']}&ticketScore=${ticketScore}`;
+    xhr.send(data_info);
+}
+
+function outLand(){
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.status == 200) {
+            //..................取回server端回傳的使用者資料
+            if (xhr.responseText.indexOf("sysError") != -1) {
+                alert("系統異常,請通知系統維護人員");
+            } else if (xhr.responseText.indexOf("還沒進場喔！") != -1) {
+                alert("還沒進場喔！");
+            } else {
+                console.log(xhr.responseText);
+                facilityPointsCheck = xhr.responseText * 1;
+            }
+        } else {
+            alert(xhr.status);
+        }
+    }
+    xhr.open("post", "php/getfacilityPoint.php", false);
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    var data_info = `str=${str}&eStr=${eStr}&ticket=${localStorage['member_useticket']}&ticketScore=${ticketScore}`;
     xhr.send(data_info);
 }
--- dev/php/gameplayaddPoints.php
+++ dev/php/gameplayaddPoints.php
@@ -1,13 +1,26 @@
 <?php
 try {
-    $str =$_POST['str'];
-    $eStr =$_POST['eStr'];
-    $ticket =$_POST['ticket'];
-    // $str ='滑水道';
-    // $eStr ='waterslide';
-    // $ticket =1;
+    $str = $_POST['str'];
+    $eStr = $_POST['eStr'];
+    $ticket = $_POST['ticket'];
+    // $str = '滑水道';
+    // $eStr = 'waterslide';
+    // $ticket = 1;
     require_once("connectWestland.php");
 
+    // 獲得倍數
+
+    $sql2 = "select bonus_multiple , team_no ,entrance_status from  ticket WHERE ticket_no = :ticket_no";
+    $ticBonus = $pdo->prepare($sql2);
+    $ticBonus->bindValue(":ticket_no", $ticket);
+    // $ticBonus->bindValue(":memid", 'me');
+    $ticBonus->execute();
+    $ticBonusRow = $ticBonus->fetch(PDO::FETCH_ASSOC);
+
+    if (!$ticBonusRow['entrance_status']) {
+        echo '還沒進場喔！';
+        die();
+    }
 
     // 獲得加幾分
 
@@ -18,22 +31,13 @@ try {
     $equBonus->execute();
     $equBonusRow = $equBonus->fetch(PDO::FETCH_ASSOC);
 
-    // 獲得倍數
-
-    $sql2 = "select bonus_multiple , team_no from ticket WHERE ticket_no = :ticket_no";
-    $ticBonus = $pdo->prepare($sql2);
-    $ticBonus->bindValue(":ticket_no", $ticket);
-    // $ticBonus->bindValue(":memid", 'me');
-    $ticBonus->execute();
-    $ticBonusRow = $ticBonus->fetch(PDO::FETCH_ASSOC);
-
-    $total = $ticBonusRow['bonus_multiple']*$equBonusRow['equ_bonus'];
+    $total = $ticBonusRow['bonus_multiple'] * $equBonusRow['equ_bonus'];
 
     // 給門票加分
 
     $sql3 = "update ticket set bounty = bounty + :bounty where ticket_no = :ticket_no";
     $tickitAddPoint = $pdo->prepare($sql3);
-    $tickitAddPoint->bindValue(":bounty",$total);
+    $tickitAddPoint->bindValue(":bounty", $total);
     $tickitAddPoint->bindValue(":ticket_no", $ticket);
     // $tickitAddPoint->bindValue(":memid", 'me');
     $tickitAddPoint->execute();
@@ -41,28 +45,36 @@ try {
     // 給團隊加分
 
 
-    $team_no =$ticBonusRow['team_no'];
+    $team_no = $ticBonusRow['team_no'];
 
-    if($team_no){
+    if ($team_no) {
         $sql4 = "update team set team_bounty = team_bounty + :bounty where team_no = :team_no";
         $teamAddPoint = $pdo->prepare($sql4);
-        $teamAddPoint->bindValue(":bounty",$total);
+        $teamAddPoint->bindValue(":bounty", $total);
         $teamAddPoint->bindValue(":team_no", $team_no);
         // $teamAddPoint->bindValue(":memid", 'me');
         $teamAddPoint->execute();
     }
 
+    // 更改設施狀態
+
+    $sql5 = "update ticket set ".$eStr." = 1 where ticket_no = :ticket_no";
+    $facility = $pdo->prepare($sql5);
+    $facility->bindValue(":ticket_no", $ticket);
+    // $facility->bindValue(":eStr", $eStr);
+    $facility->execute();
+
     // 把倍數重置
 
-    if($ticBonusRow['bonus_multiple'] != 1){
+    if ($ticBonusRow['bonus_multiple'] != 1) {
         $sql4 = "update ticket set bonus_multiple = 1 where ticket_no = :ticket_no";
         $resetTicBonus = $pdo->prepare($sql4);
         $resetTicBonus->bindValue(":ticket_no", $ticket);
         // $resetTicBonus->bindValue(":memid", 'me');
         $resetTicBonus->execute();
-        echo $equBonusRow['equ_bonus'],'|' , $ticBonusRow['bonus_multiple'],'|',$total;
-    }else{
-        echo $equBonusRow['equ_bonus'];
+        echo $total, $equBonusRow['equ_bonus'], '|', $ticBonusRow['bonus_multiple'], '|';
+    } else {
+        echo $total;
     }
 
     // 回傳加給分
