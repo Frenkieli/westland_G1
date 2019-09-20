e7783884e1749bd42a3729100b86544bd06ae360
Rex Lin <gerd3317@gmail.com>
2019-09-20 13:25:23 +0800

    更新php

--- dev/php/getMember_info.php
+++ dev/php/getMember_info.php
@@ -1,17 +1,24 @@
 <?php
     try {
     require_once("connectWestland.php");
-    $sql="SELECT`members`.`member_name`
-                ,`ticket`.`image_source`
+    $sql="SELECT`message_board`.`message_no`
+                ,`message_board`.`member_no`
+                ,`members`.`member_name`
+                ,`ticket`.`image_source` 
+                ,`team`.`team_no`
                 ,`team`.`team_name`
                 ,`ticket`.`bounty`
                 ,`team`.`mascot_image` 
-                -- ,`message_board`.`equ_no`
-                -- ,`message_board`.`message_no`
-                -- ,`message_board`.`equ_message`
-          FROM `ticket` LEFT OUTER JOIN `members` ON `ticket`.`member_no` = `members`.`member_no`
-                        LEFT OUTER JOIN `team` ON `ticket`.`team_no`=`team`.`team_no`
-                        -- LEFT OUTER JOIN `message_board` ON `ticket`.`member_no`=`message_board`.`member_no`
+
+          FROM `message_board` 
+                        LEFT OUTER JOIN `members`
+                        ON `message_board`.`member_no` =`members`.`member_no`
+                        LEFT OUTER JOIN `ticket` 
+                        ON `members`.`member_no`=`ticket`.`member_no`
+                        LEFT OUTER JOIN `team` 
+                        ON `ticket`.`team_no`=`team`.`team_no`
+          GROUP BY `message_board`.`message_no`
+        --   ORDER BY `message_board`.`message_no`
                         ";
     $Message_package = $pdo->prepare($sql);
     $Message_package->execute();
