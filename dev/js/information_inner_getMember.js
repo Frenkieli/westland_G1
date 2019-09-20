67ac6290b9e733b9040bed5ddcd6ea7870397060
Rex Lin <gerd3317@gmail.com>
2019-09-20 13:28:21 +0800

    info修改js

--- dev/js/information_inner_getMember.js
+++ dev/js/information_inner_getMember.js
@@ -15,13 +15,36 @@ window.addEventListener('load',function (){
     function getMember(jsonStr) {
         let display_member =JSON.parse(jsonStr);
         console.log(display_member);
-        for (let i = 0; i < display_member.length; i++) {
-            $(`.name_memberNo_${i}`).html(`${display_member[i].member_name}`);
-            $(`.pic_memberNo_${i}`).attr('src', `${display_member[i].image_source}`);
-            $(`.team_memberNo_${i}`).html(`${display_member[i].team_name}`);
-            $(`.bounty_memberNo_${i}`).html(`$${display_member[i].bounty}`);
-            $(`.mascot_memberNo_${i}`).html(`<img class="mascot_image" src="../dest/${display_member[i].mascot_image}" alt="客製化吉祥物">`);
+        for (let i = 0; i <=display_member.length; i++) {
+            $(`.name_memberNo_${i+1}`).html(`${display_member[i].member_name}`);
+            $(`.pic_memberNo_${i+1}`).attr('src', `${display_member[i].image_source}`);
+            $(`.team_memberNo_${i+1}`).html(`${display_member[i].team_name}`);
+            $(`.bounty_memberNo_${i+1}`).html(`$${display_member[i].bounty}`);
+            $(`.mascot_memberNo_${i+1}`).html(`<img class="mascot_image" src="../dest/${display_member[i].mascot_image}" alt="客製化吉祥物">`);
+            console.log(`.name_memberNo_${i+1}`);
+            console.log(`.pic_memberNo_${i+1}`);
+            console.log(`.team_memberNo_${i+1}`);
+            console.log(`.bounty_memberNo_${i}`);
+            console.log(`.mascot_memberNo_${i}`);
         }
+        // for (let i =display_member.length-1; i>=0; i--) {
+        //     $(`.name_memberNo_${i+1}`).html(`${display_member[i].member_name}`);
+        //     $(`.pic_memberNo_${i+1}`).attr('src', `${display_member[i].image_source}`);
+        //     $(`.team_memberNo_${i+1}`).html(`${display_member[i].team_name}`);
+        //     $(`.bounty_memberNo_${i+1}`).html(`$${display_member[i].bounty}`);
+        //     $(`.mascot_memberNo_${i+1}`).html(`<img class="mascot_image" src="../dest/${display_member[i].mascot_image}" alt="客製化吉祥物">`);
+        //     console.log(`.name_memberNo_${i+1}`);
+        //     console.log(`.pic_memberNo_${i+1}`);
+        //     console.log(`.team_memberNo_${i+1}`);
+        //     console.log(`.bounty_memberNo_${i}`);
+        //     console.log(`.mascot_memberNo_${i}`);
+        // }
     }
+    // console.log(newArr)
+    // let arr = [];
+    // display_member.forEach((obj, i) => {
+    //     arr[i] = obj['message_no'];
+    // });
+    // let newArr = [...new Set(arr)];
 }
 ,false);
