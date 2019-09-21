680e3bc4b538d095b3b5c8a95049d3e3e2f857c4
Rex Lin <gerd3317@gmail.com>
2019-09-22 02:20:30 +0800

    蓋到connectWestland.php改回來

--- dev/php/connectWestland.php
+++ dev/php/connectWestland.php
@@ -3,7 +3,10 @@
     // $dsn="mysql:host=10.120.37.7;port=3306;dbname=westland;charset=utf8";
     // $dsn="mysql:host=localhost;port=3306;dbname=products;charset=utf8";
     $user="root";
-    $password="770313";
+    $password="root";
+    // $password="icecream0510";
+    // $user="tibame123456";
+    // $password="tibame123456";
     $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
     $pdo=new PDO($dsn,$user,$password,$options);
 ?>
