4125f4a8a27d25198344bef694587ab7ad05f288
Rex Lin <gerd3317@gmail.com>
2019-09-23 17:41:43 +0800

    留言板功能分離(留言及顯示會員登入改頭像分開)

--- dev/php/clickDisplayBoard.php
+++ dev/php/clickDisplayBoard.php
@@ -4,20 +4,18 @@
 		$member_no=$_POST['member_no'];
 		$equ_no=$_POST['equ_no'];
 		$equ_message=$_POST['equ_message'];
-		// $report_status=$_POST['report_status'];
-		// $report_message=$_POST['report_message'];
 		$sql = "INSERT INTO `message_board`(`member_no`, `equ_no`, `equ_message`) 
 		VALUES ('$member_no','$equ_no','$equ_message')";
 
-
 		$starLevel=$_POST['starLevel'];
 		$sql_star="UPDATE `amusement_equipments` 
-			       SET `equ_score_total` = `equ_score_total`+$starLevel
-				   WHERE `amusement_equipments`.`equ_no` = $equ_no
+				SET `equ_score_total` = `equ_score_total`+$starLevel
+				WHERE `amusement_equipments`.`equ_no` = $equ_no
 		";
 
 		$addMessage = $pdo->prepare($sql);
 		$addMessage->execute();
+
 		$addStarlevel = $pdo->prepare($sql_star);
 		$addStarlevel->execute();
 	
