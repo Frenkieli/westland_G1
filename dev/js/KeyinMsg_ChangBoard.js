72faec5edece33f18111b618c0d17c6400b8485d
Frenkieli <sd789852t@gmail.com>
2019-09-23 18:19:18 +0800

    設施留言門票部分判斷

--- dev/js/KeyinMsg_ChangBoard.js
+++ dev/js/KeyinMsg_ChangBoard.js
@@ -1,29 +1,33 @@
 function init() {
-    let loginIsTure =sessionStorage['member_no']; //設置暫存變數
+    let loginIsTure = sessionStorage['member_no']; //設置暫存變數
     if (loginIsTure) {
-        console.log(loginIsTure)
+        // console.log(loginIsTure, '有撈到嗎?');
         let xhr = new XMLHttpRequest();
         xhr.onload = function () {
             if (xhr.status == 200) {
-                getMember(xhr.responseText);
+                // console.log(xhr.responseText);
+                if (xhr.responseText.indexOf('沒票') != -1) {
+                    // console.log('沒票喔');
+                    $(`.name_login`).html(`尚未購票`);
+                } else {
+                    let display_MEM_MSG = JSON.parse(xhr.responseText);
+                    // console.log(display_MEM_MSG, '裡面有什麼?');
+                    $(`.name_login`).html(`${display_MEM_MSG.member_name}`);
+                    $(`.pic_login`).attr('src', `${display_MEM_MSG.image_source}`);
+                    // $(`.teamName_login`).html(`${display_MEM_MSG[loginIsTure -1].team_name}`);
+                    $(`.bounty_login`).html(`$${display_MEM_MSG.bounty}`);
+                }
             }
             else {
                 window.alert(xhr.status);
             }
-        } 
-        let url="php/newKeyinBoard.php";
-            xhr.open('post', url, true);
-            xhr.send(null);
-    
-        function getMember(jsonStr) {
-            let display_MEM_MSG =JSON.parse(jsonStr);
-            console.log(display_MEM_MSG);
-            $(`.name_login`).html(`${display_MEM_MSG[loginIsTure -1].member_name}`);
-            $(`.pic_login`).attr('src', `${display_MEM_MSG[loginIsTure -1].image_source}`);
-            // $(`.teamName_login`).html(`${display_MEM_MSG[loginIsTure -1].team_name}`);
-            $(`.bounty_login`).html(`$${display_MEM_MSG[loginIsTure -1].bounty}`);
-            }
-        }else{
-            return;
         }
+        xhr.open("post", "php/newKeyinBoard.php", true);
+        xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+        var data_info = `mem_no=${loginIsTure}`;
+        xhr.send(data_info);
+    } else {
+        return;
     }
+}
+window.addEventListener('load', init, false);
--- dev/php/newKeyinBoard.php
+++ dev/php/newKeyinBoard.php
@@ -1,29 +1,20 @@
 <?php
     try {
-    require_once("connectWestland.php");
-    $sql="SELECT `members`.`member_no`
-                ,`members`.`member_name`
-                ,`ticket`.`image_source` 
-                ,`team`.`team_name`
-                ,`ticket`.`bounty`
+        require_once("connectWestland.php");
+        $mem_no = $_POST['mem_no'];
+        // $mem_no = 57;
+        $sql="select * from members a left outer join ticket b on a.member_no = b.member_no where b.member_no = :member_no order by b.ticket_no desc limit 1";
+        $Message_package = $pdo->prepare($sql);
+        $Message_package->bindValue(':member_no' , $mem_no);
+        $Message_package->execute();
 
-          FROM `members`
-                        LEFT OUTER JOIN `ticket` 
-                        ON `members`.`member_no`=`ticket`.`member_no`
-                        LEFT OUTER JOIN `team` 
-                        ON `ticket`.`team_no`=`team`.`team_no`
-          GROUP BY `members`.`member_no`
-          ORDER BY `members`.`member_no`
-                        ";
-    $Message_package = $pdo->prepare($sql);
-    $Message_package->execute();
-
-    $Message_package_array=[];
-        while($Message_package_arrayRow=$Message_package->fetch(PDO::FETCH_ASSOC)){
-            $Message_package_array[]=$Message_package_arrayRow;
+        if($Message_package->rowCount()==0){
+            echo '沒票';
+        } else{
+            $Message_packageRow=$Message_package->fetch(PDO::FETCH_ASSOC);
+            echo json_encode($Message_packageRow);
         }
-        echo json_encode($Message_package_array);
-    } 
+    }
     catch (PDOException $e) {
         echo $e->getMessage();
     }
