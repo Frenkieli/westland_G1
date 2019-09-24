6411c937f2fd85552e9e1df4f210054d9911ae0a
libby0510 <libby750510@gmail.com>
2019-09-24 16:28:20 +0800

    刪除圖片/修改活動頁php--order by

--- dev/php/getactivity_JSON.php
+++ dev/php/getactivity_JSON.php
@@ -1,10 +1,10 @@
 
 <?php
 // <!-- ====活動資訊==== -->
-
 try{
     require_once("connectWestland.php"); //引用連線
-    $sql = "select * from activity" ;
+    // $sql = "select * from activity ORDER by activity_date_start DESC" ;
+    $sql = "SELECT * FROM activity ORDER BY activity_date_start DESC" ;
     $activitys =$pdo->prepare($sql);
     $activitys->execute();
     if($activitys->rowCount()==0){
